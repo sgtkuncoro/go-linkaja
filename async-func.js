1f6315946c74510968b6e0e94c73d3aac19b9832
Sigit Kuncoro <sigit.kunc@gmail.com>
2020-10-25 13:59:45 +0700

    feat: async func

---
+++ async-func.js
@@ -0,0 +1,15 @@
+function promiseFunction() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log("calling");
+  const result = await promiseFunction();
+  console.log(result);
+}
+
+asyncCall();
